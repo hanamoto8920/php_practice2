b2c0e7ac2f699e21be23eff57223456a38096147
花本敬太 <hanamotokeita@hanamottanoiMac.sensyu.ne.jp>
2021-02-17 16:48:15 +0900

    complite

---
+++ 29.php
@@ -0,0 +1,5 @@
+<?php
+$scores = [90, 40, 100];
+
+$scores[1] = 60;
+echo $scores[1] . PHP_EOL;
---
+++ var.php
@@ -0,0 +1,10 @@
+<?php
+$scores = [
+  'first' => 90,
+  'second' => 100,
+  'third' => 200,
+];
+var_dump($scores);
+print_r($scores);
+
+echo $scores[2] . PHP_EOL;
